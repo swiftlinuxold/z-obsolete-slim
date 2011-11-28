8e4a12c1a663f43a72091a61d8cc88475190abf1
Jason Hsu <github@swiftlinux.com>
2011-11-28 12:51:24 -0600

    Added change to /etc/slim.conf

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 #! /usr/bin/env python
 
+# Check for root user login
 import os, sys
-#import subprocess
-#import sys
+if not os.geteuid()==0:
+    sys.exit("\nOnly root can run this script\n")
 
-#import sys, commands # Allows checking for root
-#import shutil # Needed for copying files
+# Get your username (not root)
+import pwd
+uname=pwd.getpwuid(1000)[0]
 
-user = os.environ['USER'] # is root or your regular user name
-username = os.environ['USERNAME'] # your regular user name, EVEN when you execute as root
-
-if user != 'root':
-	sys.exit( 'You must be root to run this script.' )
-
-# Presence of /home/mint directory indicates chroot mode in LMDE
+# The remastering process uses chroot mode.
+# Check to see if this script is operating in chroot mode.
+# /home/mint directory only exists in chroot mode
 is_chroot = os.path.exists('/home/mint')
 dir_develop=''
-
 if (is_chroot):
 	dir_develop='/usr/local/bin/develop'	
 else:
-	dir_develop='/home/'+username+'/develop'
+	dir_develop='/home/'+uname+'/develop'
+
+# Everything up to this point is common to all Python scripts called by shared-*.sh
+# =================================================================================
+
+# THIS IS THE SCRIPT FOR REPLACING THE DEFAULT LMDE LOGIN MANAGER WITH A LIGHTER ONE.
 
 # Remove GDM3 login manager
 os.system('apt-get remove -y gdm3')
@@ -28,11 +30,17 @@ os.system('apt-get remove -y gdm3')
 # Install SLiM login manager
 os.system('apt-get install -y slim')
 
+# ============================
 # Configure SLiM login manager
 
-# Create a directory for the Swift Linux theme
+# If /usr/share/slim/themes/swift/ exists, delete it.
+if (os.path.exists('/usr/share/slim/themes/swift/')):
+	os.rmdir('/usr/share/slim/themes/swift/')
+
+# Create /usr/share/slim/themes/swift/
 os.mkdir('/usr/share/slim/themes/swift/')
 
+import shutil
 # Use the panel.png file from antiX Linux
 src = dir_develop + '/ui-login/usr_share_slim_themes_swift/panel.png'
 dest = '/usr/share/slim/themes/swift/panel.png'
@@ -41,5 +49,13 @@ shutil.copy (src, dest)
 # Use the slim.theme file from antiX Linux
 src = dir_develop + '/ui-login/usr_share_slim_themes_swift/slim.theme'
 dest = '/usr/share/slim/themes/swift/slim.theme'
+shutil.copy (src, dest)
+
+# Replace /etc/slim.conf
+# Changes the available sessions (DE/WM)
+# Changes the default theme
+os.remove ('/etc/slim.conf')
+src = dir_develop + '/ui-login/etc/slim.conf'
+dest = '/etc/slim.conf'
 shutil.copy (src, dest)
 
